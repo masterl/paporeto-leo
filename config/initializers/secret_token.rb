d64c6c10abe1a39c25c38250635b49b137c0bec1
Leonardo <bigodation@gmail.com>
2013-07-22 19:59:53 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paporeto::Application.config.secret_key_base = '806a3da96b1d446317b21c6dfb58ad0b33e2c066bf7331ae58d48f938d03f77a7cfc8931eb69ee845b123e43cc72787a9c6b0bb14ce624d2f6af78c17eaae413'
