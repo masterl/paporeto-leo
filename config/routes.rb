3c1438a2f80799c17e0bb5acd9c689e6da221baa
Leonardo <bigodation@gmail.com>
2013-07-29 21:23:24 -0300

    CRUD for categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Paporeto::Application.routes.draw do
+  resources :categories, path: 'categorias'
+
   resources :articles, path: 'artigos', path_names: {new: 'criar',edit: 'editar'}
   root 'articles#index'
 end
