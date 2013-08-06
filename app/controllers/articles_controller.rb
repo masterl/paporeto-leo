87268d0100e18b8a7903de0dd442d8ccd63813f6
Leonardo <bigodation@gmail.com>
2013-08-05 22:04:02 -0300

    featured article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.limit(8)
+    @featured = Article.where(featured: true).first
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130731223548) do
+ActiveRecord::Schema.define(version: 20130806003637) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20130731223548) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
+    t.boolean  "featured",    default: false
   end
 
   create_table "categories", force: true do |t|
